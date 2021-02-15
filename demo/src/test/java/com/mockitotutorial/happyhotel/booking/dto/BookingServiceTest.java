9dbe65bc6ebb08d36a545d3d04ff4e8ac0140b03
viveksoni100 <viveksoni100@gmail.com>
2021-02-15 11:11:07 +0530

    mockito course is over

--- demo/src/test/java/com/mockitotutorial/happyhotel/booking/dto/BookingServiceTest.java
+++ demo/src/test/java/com/mockitotutorial/happyhotel/booking/dto/BookingServiceTest.java
@@ -1,8 +1,10 @@
 package com.mockitotutorial.happyhotel.booking.dto;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.function.Executable;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.util.Arrays;
@@ -12,33 +14,43 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class BookingServiceTest {
 
+    @InjectMocks
     private BookingService bookingService;
-
+    @Mock
     private PaymentService paymentServiceMock;
+    @Mock
     private RoomService roomServiceMock;
+    @Spy
     private BookingDAO bookingDAOMock;
+    @Mock
     private MailSender mailSenderMock;
+    @Captor
+    private ArgumentCaptor<Double> doubleCaptor;
 
-    @BeforeEach
+    /*@BeforeEach
+    // if we are not using @BeforeEach then we can use @Mock, @Spy annotations on fields
     void setup() {
         this.paymentServiceMock = mock(PaymentService.class);
         this.roomServiceMock = mock(RoomService.class);
         this.bookingDAOMock = spy(BookingDAO.class);    // partial mock called spies
-        /*this.bookingDAOMock = mock(BookingDAO.class);*/
+        *//*this.bookingDAOMock = mock(BookingDAO.class);*//*
         this.mailSenderMock = mock(MailSender.class);
         this.bookingService = new BookingService(paymentServiceMock, roomServiceMock,
                 bookingDAOMock, mailSenderMock); //we have provided mocks of the dependencies for BS
 
         //        this is called nice return values
-        /*System.out.println("List returned : " + roomServiceMock.getAvailableRooms());
+        *//*System.out.println("List returned : " + roomServiceMock.getAvailableRooms());
         System.out.println("Object returned : " + roomServiceMock.findAvailableRoomId(null));
-        System.out.println("Primitive returned : " + roomServiceMock.getRoomCount());*/
+        System.out.println("Primitive returned : " + roomServiceMock.getRoomCount());*//*
 
+        this.doubleCaptor = ArgumentCaptor.forClass(Double.class);
 
-    }
+    }*/
 
     @Test
     void should_CountAvailablePlaces() {
@@ -196,6 +208,95 @@ class BookingServiceTest {
 
         bookingService.cancelBooking(bookingId);    // will return null pointer without doReturn(...)
     }
+
+    // argument captors
+    @Test
+    void should_PayCorrectPrice_When_InputOK() {
+
+        BookingRequest bookingRequest = new BookingRequest("1", LocalDate.of(2020, 01, 01),
+                LocalDate.of(2020, 01, 05), 2, true);
+
+        bookingService.makeBooking(bookingRequest);
+
+        verify(paymentServiceMock, times(1)).pay(eq(bookingRequest), doubleCaptor.capture());
+        double capturedArg = doubleCaptor.getValue();
+        assertEquals(400.0, capturedArg);
+    }
+
+    @Test
+    void should_PayCorrectPrices_When_MultipleCalls() {
+
+        BookingRequest bookingRequest = new BookingRequest("1", LocalDate.of(2020, 01, 01),
+                LocalDate.of(2020, 01, 05), 2, true);
+        BookingRequest bookingRequest2 = new BookingRequest("1", LocalDate.of(2020, 01, 01),
+                LocalDate.of(2020, 01, 02), 2, true);
+        List<Double> expectedValues = Arrays.asList(400.0, 100.0);
+
+        bookingService.makeBooking(bookingRequest);
+        bookingService.makeBooking(bookingRequest2);
+
+        verify(paymentServiceMock, times(2)).pay(any(), doubleCaptor.capture());
+        List<Double> capturedArg = doubleCaptor.getAllValues();
+        assertEquals(expectedValues, capturedArg);
+    }
+
+    // BDD example / Behavior Driven Development
+    @Test
+    void should_CountAvailablePlaces_When_OneRoomAvailable_BDD() {
+
+        given(this.roomServiceMock.getAvailableRooms())
+                .willReturn(Collections.singletonList(new Room("Room 1", 2)));  // 5 will fail
+
+        int expected = 2;
+
+        int actual = bookingService.getAvailablePlaceCount();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void should_InvokePayment_When_Prepaid_BDD() {
+
+        BookingRequest bookingRequest = new BookingRequest("1", LocalDate.of(2020, 01, 01),
+                LocalDate.of(2020, 01, 05), 2, true);
+
+        bookingService.makeBooking(bookingRequest); // here we want to verify that paymentService.pay(bookingRequest, price) method is really called or not
+
+        then(paymentServiceMock).should().pay(bookingRequest, 400.0);
+    }
+
+    // strict stubbing
+    @Test
+    void should_InvokePayment_When_Prepaid_SS() {
+
+        BookingRequest bookingRequest = new BookingRequest("1", LocalDate.of(2020, 01, 01),
+                LocalDate.of(2020, 01, 05), 2, true);
+        lenient().when(paymentServiceMock.pay(any(), anyDouble())).thenReturn("1");   // this will be the case of stubbing (mock is not going to used)
+        // lenient() will solve the stub
+
+        bookingService.makeBooking(bookingRequest); // here we want to verify that paymentService.pay(bookingRequest, price) method is really called or not
+
+        // just wanna check, no exception is thrown
+
+    }
+
+    // test static methods using mockito 3.4*
+    @Test
+    void should_CalculateCorrectPrice() {
+
+        try (MockedStatic<CurrencyConverter> converterMockedStatic = mockStatic(CurrencyConverter.class)) {
+            BookingRequest bookingRequest = new BookingRequest("1", LocalDate.of(2020, 01, 01),
+                    LocalDate.of(2020, 01, 05), 2, true);
+            double expected = 400.0;
+            converterMockedStatic.when(() -> CurrencyConverter.toEuro(anyDouble())).thenReturn(400.0);
+
+            double actual = bookingService.calculatePriceEuro(bookingRequest);
+
+            assertEquals(expected, actual);
+        }
+    }
+
+    // Mockito Answers / thenAnswer
 
     @Test
     void cancelBooking() {
